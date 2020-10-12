16ec11f476a6872b3fc88f61fa9e9dbe18adef1e
Dennis <denismutai95@gmail.com>
2020-10-12 22:48:36 +0300

    create functionality of user's credential

---
+++ run.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3.6
+from user import User
+from credential import Credential
+import random
+
+
+def create_user(fname, lname, phone, email):
+    """
+    Function to create a new user
+    """
+    new_user = User(fname, lname, phone, email)
+    return new_user
+
+
+def create_credential(uname, pword, email):
+    """
+    Function to create new user credentials
+    """
+    new_credential = Credential(uname, pword, email)
+    return new_credential
+
+
+def save_user(user):
+    """
+    Function to save user
+    """
+    user.save_user_details()
+def verify_user(first_name,pword):
+    checking_user = Credential.check_user(first_name,pword)
+    return checking_user
+
+    
